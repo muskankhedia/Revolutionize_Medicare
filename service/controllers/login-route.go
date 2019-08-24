cde8a49b084baf4f9ce51d9abf1f40fb68237e3e
C-Anirudh <canirudh16@gmail.com>
2019-08-24 19:29:33 +0530

    move from profileHandler to LoginHandler

--- service/controllers/login-route.go
+++ service/controllers/login-route.go
@@ -1,17 +1,40 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 //LoginHandler returns the medical history details of the patient id
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("In login handler")
+	var present = "false"
 	// prevent CORS error
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	r.ParseForm()
-	
-	fmt.Println("Reached1")
+	var form SignupForm
+	var users []SignupForm
+
+	if err := parseForm(r, &form); err != nil {
+		panic(err)
+	}
+	jsonByteValue, err := ioutil.ReadFile("datastore/users.json")
+	if err != nil {
+		panic(err)
+	}
+	if len(jsonByteValue) != 0 {
+		err = json.Unmarshal(jsonByteValue, &users)
+		if err != nil {
+			panic(err)
+		}
+		for i := 0; i < len(users); i++ {
+			if form.PatientID == users[i].PatientID {
+				present = "true"
+			}
+		}
+	}
+	log.Println(present)
+	w.Write([]byte(present))
 }
