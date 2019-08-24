13bb7d67c31a700cad8ae98759f72dddaddf156c
muskankhedia <muskan.khedia2000@gmail.com>
2019-08-24 16:55:30 +0530

    blockchain added to store data

--- service/controllers/add_data-route.go
+++ service/controllers/add_data-route.go
@@ -3,16 +3,152 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
 )
+
+// Block represents each 'item' in the blockchain
+type Block struct {
+	Index     int
+	Timestamp string
+	PatientID  int 
+	Hash      string
+	PrevHash  string
+	Event string
+	Medicine []string
+	TimeSFO int 
+}
+
+// Blockchain is a series of validated Blocks
+var Blockchain []Block
+
+// Message takes incoming JSON payload for writing heart rate
+type Message struct {
+	PatientID  int 
+	event string
+	medicine []string
+	timeSFO int 
+}
+
+var mutex = &sync.Mutex{}
+
+var patientID, timeSinceFirstOccurence int
+var event string
+var medicine []string
 
 //AddDataHandler adds the data into the user details 
 func AddDataHandler(w http.ResponseWriter, r *http.Request) {
 
 	// prevent CORS error
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	r.ParseForm()
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	// r.ParseForm()
 
+	// patientID, err := strconv.Atoi(r.FormValue("patientid"))
+	// event = r.FormValue("event")
+	// medicine = strings.Split(r.FormValue("medicine"), ",")
+	// timeSinceFirstOccurence, err := strconv.Atoi(r.FormValue("time_since_first_occurance"))
 	
-	fmt.Println("Reached Add Data")
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{}
+		genesisBlock = Block{0, t.String(), 0, calculateHash(genesisBlock), "", "", []string{}, 0}
+		spew.Dump(genesisBlock)
+
+		mutex.Lock()
+		Blockchain = append(Blockchain, genesisBlock)
+		mutex.Unlock()
+	}()
+
+	w.Header().Set("Content-Type", "application/json")
+	var msg Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&msg); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	mutex.Lock()
+	prevBlock := Blockchain[len(Blockchain)-1]
+	newBlock := generateBlock(prevBlock, msg.PatientID)
+
+	if isBlockValid(newBlock, prevBlock) {
+		Blockchain = append(Blockchain, newBlock)
+		spew.Dump(Blockchain)
+	}
+	mutex.Unlock()
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// make sure block is valid by checking index, and comparing the hash of the previous block
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+// SHA256 hasing
+func calculateHash(block Block) string {
+	record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.PatientID) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+// create a new block using previous block's hash
+func generateBlock(oldBlock Block, PatientID int) Block {
+
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.PatientID = PatientID
+	newBlock.event = event
+	newBlock.Medicine = medicine
+	newBlock.TimeSFO = timeSinceFirstOccurence
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock
 }
